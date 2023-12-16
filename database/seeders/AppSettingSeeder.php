77461c355572ffdfd55ee30e1b44a4d038be5ab2
asiifdev <asiif.anwar3@gmail.com>
2023-12-16 23:35:38 +0700

    Adding App Setting Seeder

---
+++ database/seeders/AppSettingSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\AppSetting;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AppSettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        AppSetting::create([
+            'business_category' => "Company",
+            "default_theme_enum" => "light"
+        ]);
+    }
+}
